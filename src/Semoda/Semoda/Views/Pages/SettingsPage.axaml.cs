3deeb912ffe8041c06426e3502e1fea667737f17
TheTanic <victor@hoeher.de>
2024-06-27 22:47:30 +0200

    Rename DashboardPageView to DashboardPage Add SettingsPage Add SettingsPageViewModel Add Settings Icon Add MenuItem for SettingsPage

---
+++ src/Semoda/Semoda/Views/Pages/SettingsPage.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+
+namespace Semoda.Views.Pages;
+
+/// <summary>
+/// Page to handle the settings for the application.
+/// </summary>
+public partial class SettingsPage : UserControl
+{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}
