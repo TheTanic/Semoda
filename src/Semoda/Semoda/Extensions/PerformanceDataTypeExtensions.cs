17ea74c877ecb0acf364351cfd1b5f86f6fccb2e
TheTanic <victor@hoeher.de>
2024-06-30 16:35:34 +0200

    Reogranize Extension

--- src/Semoda/Semoda/Extensions/PerformanceDataTypeExtensions.cs
+++ src/Semoda/Semoda/Extensions/PerformanceDataTypeExtensions.cs
@@ -11,37 +11,6 @@ namespace Semoda.Extensions
     public static class PerformanceDataTypeExtensions
     {
         /// <summary>
-        /// Convert the data type to a <see cref="IPerformanceDataCollector"/>
-        /// </summary>
-        /// <param name="dataType">Type to convert</param>
-        /// <returns>The corresponding <see cref="IPerformanceDataCollector"/> of the dataType.
-        /// <see langword="null"/> if there is no linked <see cref="IPerformanceDataCollector"/>.</returns>
-        public static IPerformanceDataCollector? ToPerformanceDataCollector(this PerformanceDataType dataType)
-        {
-            switch (dataType)
-            {
-                case PerformanceDataType.TotalCPUUtilization:
-                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                        return new PerformanceDataCollectorWindows(new PerformanceCounter("Processor Information", "% Processor Utility", "_Total"));
-                    break;
-
-                case PerformanceDataType.TotalRAMAvailable:
-                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                        return new PerformanceDataCollectorWindows(new PerformanceCounter("Memory", "Available MBytes"));
-                    break;
-
-                case PerformanceDataType.CPUTemperature:
-                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-                        return new PerformanceDataCollectorWindows(new PerformanceCounter("Thermal Zone Information", "Temperature", "_TZ.THRM"));
-                    break;
-
-                default:
-                    return null;
-            }
-            return null;
-        }
-
-        /// <summary>
         /// Get the default unit for the <see cref="PerformanceDataType"/>
         /// </summary>
         /// <param name="dataType">Type of which the unit should be gathered.</param>
@@ -62,6 +31,34 @@ namespace Semoda.Extensions
                 default:
                     return "";
             }
+        }
+
+        /// <summary>
+        /// Convert the data type to a <see cref="IPerformanceDataCollector"/>
+        /// </summary>
+        /// <param name="dataType">Type to convert</param>
+        /// <returns>The corresponding <see cref="IPerformanceDataCollector"/> of the dataType.
+        /// <see langword="null"/> if there is no linked <see cref="IPerformanceDataCollector"/>.</returns>
+        public static IPerformanceDataCollector? ToPerformanceDataCollector(this PerformanceDataType dataType)
+        {
+            switch (dataType)
+            {
+                case PerformanceDataType.TotalCPUUtilization:
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        return new PerformanceDataCollectorWindows(new PerformanceCounter("Processor Information", "% Processor Utility", "_Total"));
+                    break;
+
+                case PerformanceDataType.TotalRAMAvailable:
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                        return new PerformanceDataCollectorWindows(new PerformanceCounter("Memory", "Available MBytes"));
+                    break;
+
+                case PerformanceDataType.CPUTemperature:
+
+                default:
+                    return null;
+            }
+            return null;
         }
     }
 }
